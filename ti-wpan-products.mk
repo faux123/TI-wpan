7559dccd09ccc6808d78b8f49ae5e78737f5609d
Pavan Savoy <pavan_savoy@ti.com>
2011-08-11 17:25:36 -0500

    wpan: firmware: add firmware folder
    firmware which was previously not released along with source code, now is part
    of GIT repository being copied when built along with other wpan packages.
    
    Change-Id: I4ad28d6ed5148f66530cd7b459c411a812bcc7b9
    Signed-off-by: Pavan Savoy <pavan_savoy@ti.com>

--- ti-wpan-products.mk
+++ ti-wpan-products.mk
@@ -1,4 +1,6 @@
 # This file lists the firmware, software that are specific to
 # WiLink connectivity chip on OMAPx platforms.
 
-PRODUCT_PACKAGES += uim-sysfs
+PRODUCT_PACKAGES += uim-sysfs \
+	TIInit_10.6.15.bts \
+	TIInit_10.4.27.bts
