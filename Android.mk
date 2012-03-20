b3ca064110527b4e5f7298fe6c6d7ca3960fc041
Pavan Savoy <pavan_savoy@ti.com>
2012-03-20 16:05:46 -0500

    blueti: copy main.conf for blueti
    blueti enables LE configuration read by bluetoothd during BT turn on, and also
    enables the support for GATT server via main.conf configuration.
    
    Change-Id: I4fd515bcb34691a8f370e55ba12a7fede43ebe1c
    Signed-off-by: Pavan Savoy <pavan_savoy@ti.com>

--- Android.mk
+++ Android.mk
@@ -1,2 +1,16 @@
 #wpan utilties and TI ST user space manager
 include $(call first-makefiles-under,$(call my-dir))
+
+
+LOCAL_PATH := $(call my-dir)
+ifeq ($(BLUETI_ENHANCEMENT), true)
+
+#install main.conf
+include $(CLEAR_VARS)
+LOCAL_MODULE := main.conf
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT)/etc/bluetooth
+LOCAL_SRC_FILES := ../../system/bluetooth/data/main.conf
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_PREBUILT)
+endif
