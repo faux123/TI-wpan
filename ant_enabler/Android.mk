a594c350a08152197ccf98a5c2483983baf5e863
Amir Hadar <amir.hadar.ti@ti.com>
2012-03-20 16:02:12 -0500

    blueti: ant: Add ant_enabler module
    This module is reponsible on downloading the ANT script
    and switching between ANT and BLE cortext scripts in 12xx chips.
    
    Change-Id: Iaf2626c8a84eeb8fc5373584df952739fc3287b6
    Signed-off-by: Pavan Savoy <pavan_savoy@ti.com>

---
+++ ant_enabler/Android.mk
@@ -0,0 +1,28 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+#
+# ANT enabler Application
+#
+
+LOCAL_C_INCLUDES:= ant_enabler.h
+
+LOCAL_SRC_FILES:= \
+	ant_enabler.c
+LOCAL_PRELINK_MODULE := false
+
+
+LOCAL_CFLAGS:= -g -c -W -Wall -O2 -D_POSIX_SOURCE
+LOCAL_C_INCLUDES:=\
+	$(call include-path-for, bluez)/src \
+	$(call include-path-for, bluez)/lib
+
+
+LOCAL_SHARED_LIBRARIES:= libnetutils libcutils libbluetoothd libbluetooth
+LOCAL_MODULE:=libantenable
+LOCAL_MODULE_TAGS:= eng
+
+#include $(BUILD_EXECUTABLE)
+include $(BUILD_SHARED_LIBRARY)
+
+
