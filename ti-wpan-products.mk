6edd019097eb78c41c8ad339b492f5dc9e10c02a
Pavan Savoy <pavan_savoy@ti.com>
2012-05-07 12:39:56 -0500

    wpan: add common rule for firmware files
    With an intent to maintain different source name (which would have included
    sub-version) & destination name, each file was individually copied to
    file-system.
    Since we no longer need different src/dest names, have a generic rule to copy
    the firmware files to target.
    
    Patch modified from original - Mark Lifshits <lifshitsm@ti.com>
    Signed-off-by: Pavan Savoy <pavan_savoy@ti.com>
    
    Change-Id: Ibaa3e69a6470ca27ea0ebd6149643199270fb3de

--- ti-wpan-products.mk
+++ ti-wpan-products.mk
@@ -28,15 +28,7 @@ PRODUCT_PACKAGES += \
 
 #copy firmware
 PRODUCT_COPY_FILES += \
-  device/ti/proprietary-open/wl12xx/wpan/bluetooth/TIInit_10.6.15.bts:system/etc/firmware/TIInit_10.6.15.bts \
-  device/ti/proprietary-open/wl12xx/wpan/bluetooth/TIInit_7.2.31.bts:system/etc/firmware/TIInit_7.2.31.bts \
-  device/ti/proprietary-open/wl12xx/wpan/bluetooth/TIInit_7.6.15.bts:system/etc/firmware/TIInit_7.6.15.bts \
-  device/ti/proprietary-open/wl12xx/wpan/fmradio/fmc_ch8_1283.2.bts:system/etc/firmware/fmc_ch8_1283.2.bts \
-  device/ti/proprietary-open/wl12xx/wpan/fmradio/fm_rx_ch8_1283.2.bts:system/etc/firmware/fm_rx_ch8_1283.2.bts \
-  device/ti/proprietary-open/wl12xx/wpan/fmradio/fm_tx_ch8_1283.2.bts:system/etc/firmware/fm_tx_ch8_1283.2.bts \
-  device/ti/proprietary-open/wl12xx/wpan/fmradio/fmc_init_1273.2.bts:system/etc/firmware/fmc_init_1273.2.bts \
-  device/ti/proprietary-open/wl12xx/wpan/fmradio/fm_rx_init_1273.2.bts:system/etc/firmware/fm_rx_init_1273.2.bts \
-  device/ti/proprietary-open/wl12xx/wpan/fmradio/fm_tx_init_1273.2.bts:system/etc/firmware/fm_tx_init_1273.2.bts \
-  device/ti/proprietary-open/wl12xx/wpan/fmradio/fm_tx_ch8_1273.1.bts:system/etc/firmware/fm_tx_ch8_1273.1.bts \
-  device/ti/proprietary-open/wl12xx/wpan/fmradio/fm_tx_ch8_1273.2.bts:system/etc/firmware/fm_tx_ch8_1273.2.bts \
   system/bluetooth/data/main.conf:system/etc/bluetooth/main.conf
+
+_all_fw_files := $(wildcard device/ti/proprietary-open/wl12xx/wpan/*/*.bts)
+PRODUCT_COPY_FILES += $(foreach f, $(_all_fw_files),$(f):system/etc/firmware/$(notdir $(f)) )
